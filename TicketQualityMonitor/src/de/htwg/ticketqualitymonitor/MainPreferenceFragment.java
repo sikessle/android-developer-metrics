e767613d501143420b2524fcf9a89425a7028416
Simon Kessler <simon.kessler@icloud.com>
2015-10-31 10:40:17 +0100

    Improved user experience by allowing the user to easily retry the projects loading.

--- TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/MainPreferenceFragment.java
+++ TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/MainPreferenceFragment.java
@@ -4,6 +4,8 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import de.htwg.ticketqualitymonitor.model.JiraApi;
@@ -11,8 +13,8 @@ import de.htwg.ticketqualitymonitor.model.JiraApi;
 /**
  * This fragment shows the preferences for the first header.
  */
-public class MainPreferenceFragment extends PreferenceFragment
-		implements OnSharedPreferenceChangeListener {
+public class MainPreferenceFragment extends PreferenceFragment implements
+OnSharedPreferenceChangeListener, OnPreferenceClickListener {
 
 	private JiraApi api;
 
@@ -22,7 +24,7 @@ public class MainPreferenceFragment extends PreferenceFragment
 
 		// Listen to preference changes
 		PreferenceManager.getDefaultSharedPreferences(getActivity())
-				.registerOnSharedPreferenceChangeListener(this);
+		.registerOnSharedPreferenceChangeListener(this);
 		api = JiraApiFactory.createInstance(getActivity());
 
 		// Load the preferences from an XML resource
@@ -32,10 +34,17 @@ public class MainPreferenceFragment extends PreferenceFragment
 	}
 
 	@Override
-	public void onSharedPreferenceChanged(
-			SharedPreferences sharedPreferences, String key) {
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
 		api = JiraApiFactory.createInstance(getActivity());
 		initProjectList();
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		((ListPreference) preference).getDialog().dismiss();
+		initProjectList();
+		return false;
 	}
 
 	private void initProjectList() {
@@ -44,9 +53,13 @@ public class MainPreferenceFragment extends PreferenceFragment
 		final ListPreference prefList = (ListPreference) findPreference(keyProjectList);
 		// Shows loading indicator
 		prefList.setSummary(getString(R.string.loading));
+		// Will be set by the error listener if an error occurs, so it can be
+		// handled in this class by allowing the user to reload the list by
+		// clicking on the projects preference list.
+		prefList.setOnPreferenceClickListener(null);
 
 		api.getProjects(new ProjectsListener(prefList),
-				new ProjectsErrorListener(prefList, errorMessage));
+				new ProjectsErrorListener(this, prefList, errorMessage));
 	}
 
 }
--- TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/ProjectsErrorListener.java
+++ TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/ProjectsErrorListener.java
@@ -1,15 +1,19 @@
 package de.htwg.ticketqualitymonitor;
 
 import android.preference.ListPreference;
+import android.preference.Preference.OnPreferenceClickListener;
 
-import com.android.volley.VolleyError;
 import com.android.volley.Response.ErrorListener;
+import com.android.volley.VolleyError;
 
 public class ProjectsErrorListener implements ErrorListener {
 	private ListPreference prefList;
 	private String errorMessage;
+	private OnPreferenceClickListener errorClickListener;
 
-	public ProjectsErrorListener(ListPreference prefList, String errorMessage) {
+	public ProjectsErrorListener(OnPreferenceClickListener errorClickListener,
+			ListPreference prefList, String errorMessage) {
+		this.errorClickListener = errorClickListener;
 		this.prefList = prefList;
 		this.errorMessage = errorMessage;
 	}
@@ -19,7 +23,7 @@ public class ProjectsErrorListener implements ErrorListener {
 		prefList.setSummary(errorMessage);
 		prefList.setEntries(new String[] {});
 		prefList.setEntryValues(new String[] {});
-		// prefList.setOnPreferenceClickListener(onPreferenceClickListener);
+		prefList.setOnPreferenceClickListener(errorClickListener);
 	}
 
 }
