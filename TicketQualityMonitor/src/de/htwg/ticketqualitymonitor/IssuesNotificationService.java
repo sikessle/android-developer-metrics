7017d15dc254fc2062d7c5175fb9c44cfe9f2e3e
Simon Kessler <simon.kessler@icloud.com>
2015-11-04 15:31:35 +0100

    Basic notification dummy

--- TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/IssuesNotificationService.java
+++ TicketQualityMonitor/src/de/htwg/ticketqualitymonitor/IssuesNotificationService.java
@@ -7,9 +7,13 @@ import de.htwg.ticketqualitymonitor.model.JiraApi;
 import de.htwg.ticketqualitymonitor.model.JiraApiFactory;
 import de.htwg.ticketqualitymonitor.model.JiraIssue;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 /**
@@ -31,7 +35,8 @@ public class IssuesNotificationService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		Log.i(IssuesNotificationService.class.getSimpleName(), "onHandleIntent called.");
+		Log.i(IssuesNotificationService.class.getSimpleName(),
+				"onHandleIntent called.");
 
 		setUpApiAndProjectKey();
 		loadIssues();
@@ -50,8 +55,27 @@ public class IssuesNotificationService extends IntentService {
 	}
 
 	private void loadIssues() {
+		sendNotification();
 		// TODO notify..
-		// api.getAssignedInProgressIssuess(projectKey, issuesListener, errorListener);
+		// api.getAssignedInProgressIssuess(projectKey, issuesListener,
+		// errorListener);
+	}
+
+	private void sendNotification() {
+		final PendingIntent startAppIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, MainActivity.class), 0);
+
+		final Notification notifi = new NotificationCompat.Builder(this)
+				.setContentTitle(getString(R.string.notification_title))
+				.setContentText(getString(R.string.notification_description))
+				.setContentIntent(startAppIntent)
+				.setSmallIcon(R.drawable.ic_launcher).build();
+		final NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+		// hide the notification after its selected
+		notifi.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		manager.notify(0, notifi);
 	}
 
 }
